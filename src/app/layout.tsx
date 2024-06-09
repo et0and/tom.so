b91556eb14a0da8340ae716a4a2c57bc96f525ae
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2024-06-09 23:00:01 +1200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Head from "next/head";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import Navbar from "@/components/ui/navbar";
@@ -59,7 +60,27 @@ export default function RootLayout({
       )}
     >
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <meta property="og:image" content="/og.png" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:image" content="/og.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body className="antialiased w-full px-4 my-8 lg:mx-auto">
         <main className="min-h-screen flex flex-col">
