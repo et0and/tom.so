4d42aa8beb45b1e0d4131b4e4fc781458e4087dc
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2024-06-09 22:51:30 +1200

    Update robot.ts

--- src/app/robot.ts
+++ src/app/robot.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
+      allow: '/api/og/*',
     },
-    sitemap: 'https://tom.so/sitemap.xml',
   }
 }
