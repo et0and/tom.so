f257180a6b9ba4b1a49541926895a05f4bb9b6cf
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2024-06-27 23:30:30 +1200

    More hovercards and a short blog post

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,18 +1,14 @@
 import Link from "next/link";
 import type { Metadata } from "next";
-import { CalendarDays } from "lucide-react"
- 
-import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-} from "@/components/ui/avatar"
-import { Button } from "@/components/ui/button"
+import { CalendarDays } from "lucide-react";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
 import {
   HoverCard,
   HoverCardContent,
   HoverCardTrigger,
-} from "@/components/ui/hover-card"
+} from "@/components/ui/hover-card";
 
 export const metadata: Metadata = {
   title: "About",
@@ -29,8 +25,39 @@ export default function About() {
         am working as a web producer and developer in Pōneke, Te
         Whanganui-a-Tara. Prior to this, I taught design, art and digital
         technology at Takapuna Grammar School, where I also worked on the
-        development of Te Wāhi Auaha (a school maker space and innovation
-        space).
+        development of{" "}
+        <HoverCard>
+          <HoverCardTrigger asChild>
+            <Link
+              href="/work/te-wahi"
+              className="underline hover:text-blue-700 transition-colors duration-200"
+            >
+              Te Wāhi Auaha
+            </Link>
+          </HoverCardTrigger>
+          <HoverCardContent className="w-80">
+            <div className="flex justify-between space-x-4">
+              <Avatar>
+                <AvatarImage src="/twa.png" />
+                <AvatarFallback>Te Wāhi</AvatarFallback>
+              </Avatar>
+              <div className="space-y-1">
+                <p className="text-sm font-semibold">Te Wāhi Auaha</p>
+                <p className="text-sm">
+                  Te Wāhi Auaha is the school maker space at Takapuna Grammar
+                  School.
+                </p>
+                <div className="flex items-center pt-2">
+                  <CalendarDays className="mr-2 h-4 w-4 opacity-70" />{" "}
+                  <span className="text-sm text-muted-foreground">
+                    Joined January 2021
+                  </span>
+                </div>
+              </div>
+            </div>
+          </HoverCardContent>
+        </HoverCard>
+        &nbsp;(a school maker space and innovation space).
       </p>
       <p className="pb-4">
         My experience in making learning more accessible and equitable across a
@@ -39,133 +66,181 @@ export default function About() {
       </p>
       <p className="pb-4">
         Previously I studied at the{" "}
-      <HoverCard>
-      <HoverCardTrigger asChild>
-      <Link
-          href="https://elamartists.ac.nz"
-          className="underline hover:text-blue-700 transition-colors duration-200"
-        >
-          Elam School of Fine Arts
-        </Link>
-      </HoverCardTrigger>
-      <HoverCardContent className="w-80">
-        <div className="flex justify-between space-x-4">
-          <Avatar>
-            <AvatarImage src="/uoa.jpg" />
-            <AvatarFallback>Elam</AvatarFallback>
-          </Avatar>
-          <div className="space-y-1">
-            <p className="text-sm font-medium">Elam School of Fine Arts</p>
-            <p className="text-sm">
-            The Elam School of Fine Arts, founded by John Edward Elam, is part of the Faculty of Creative Arts and Industries at the University of Auckland. It offered the first Bachelor of Fine Arts programme in New Zealand starting in 1967.
-            </p>
-            <div className="flex items-center pt-2">
-              <CalendarDays className="mr-2 h-4 w-4 opacity-70" />{" "}
-              <span className="text-sm text-muted-foreground">
-                Joined March 2014
-              </span>
+        <HoverCard>
+          <HoverCardTrigger asChild>
+            <Link
+              href="https://elamartists.ac.nz"
+              className="underline hover:text-blue-700 transition-colors duration-200"
+            >
+              Elam School of Fine Arts
+            </Link>
+          </HoverCardTrigger>
+          <HoverCardContent className="w-80">
+            <div className="flex justify-between space-x-4">
+              <Avatar>
+                <AvatarImage src="/uoa.jpg" />
+                <AvatarFallback>Elam</AvatarFallback>
+              </Avatar>
+              <div className="space-y-1">
+                <p className="text-sm font-semibold">
+                  Elam School of Fine Arts
+                </p>
+                <p className="text-sm">
+                  The Elam School of Fine Arts, founded by John Edward Elam, is
+                  part of the Faculty of Creative Arts and Industries at the
+                  University of Auckland. It offered the first Bachelor of Fine
+                  Arts programme in New Zealand starting in 1967.
+                </p>
+                <div className="flex items-center pt-2">
+                  <CalendarDays className="mr-2 h-4 w-4 opacity-70" />{" "}
+                  <span className="text-sm text-muted-foreground">
+                    Joined March 2014
+                  </span>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </HoverCardContent>
-    </HoverCard>, where I also briefly taught the first and second year studio programme.
+          </HoverCardContent>
+        </HoverCard>
+        , where I also briefly taught the first and second year studio
+        programme.
       </p>
       <p className="pb-4">
         Find me on{" "}
-      <HoverCard>
-      <HoverCardTrigger asChild>
-      <Link
-          href="https://are.na/tom"
-          className="underline hover:text-blue-700 transition-colors duration-200"
-        >
-          are.na
-        </Link>
-      </HoverCardTrigger>
-      <HoverCardContent className="w-80">
-        <div className="flex justify-between space-x-4">
-          <Avatar>
-            <AvatarImage src="/arena.jpg" />
-            <AvatarFallback>Are.na</AvatarFallback>
-          </Avatar>
-          <div className="space-y-1">
-            <p className="text-sm font-medium">Are.na</p>
-            <p className="text-sm">
-            Are.na is an online social networking community and creative research platform founded by Charles Broskoski, Daniel Pianetti, Chris Barley, and Chris Sherron.
-            </p>
-            <div className="flex items-center pt-2">
-              <CalendarDays className="mr-2 h-4 w-4 opacity-70" />{" "}
-              <span className="text-sm text-muted-foreground">
-                Joined January 2018
-              </span>
+        <HoverCard>
+          <HoverCardTrigger asChild>
+            <Link
+              href="https://are.na/tom"
+              className="underline hover:text-blue-700 transition-colors duration-200"
+            >
+              are.na
+            </Link>
+          </HoverCardTrigger>
+          <HoverCardContent className="w-80">
+            <div className="flex justify-between space-x-4">
+              <Avatar>
+                <AvatarImage src="/arena.jpg" />
+                <AvatarFallback>Are.na</AvatarFallback>
+              </Avatar>
+              <div className="space-y-1">
+                <p className="text-sm font-semibold">Are.na</p>
+                <p className="text-sm">
+                  Are.na is an online social networking community and creative
+                  research platform founded by Charles Broskoski, Daniel
+                  Pianetti, Chris Barley, and Chris Sherron.
+                </p>
+                <div className="flex items-center pt-2">
+                  <CalendarDays className="mr-2 h-4 w-4 opacity-70" />{" "}
+                  <span className="text-sm text-muted-foreground">
+                    Joined January 2018
+                  </span>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </HoverCardContent>
-    </HoverCard>
+          </HoverCardContent>
+        </HoverCard>
         ,{" "}
         <HoverCard>
-      <HoverCardTrigger asChild>
-      <Link
-          href="https://read.cv/hackshaw"
-          className="underline hover:text-blue-700 transition-colors duration-200"
-        >
-          Read.cv
-        </Link>
-      </HoverCardTrigger>
-      <HoverCardContent className="w-80">
-        <div className="flex justify-between space-x-4">
-          <Avatar>
-            <AvatarImage src="/readcv.svg" />
-            <AvatarFallback>Read.cv</AvatarFallback>
-          </Avatar>
-          <div className="space-y-1">
-            <p className="text-sm font-medium">Read.cv</p>
-            <p className="text-sm">
-            Read.cv is a show, don&apos;t tell professional platform to form beautiful profiles and make meaningful connections with people and teams.
-            </p>
-            <div className="flex items-center pt-2">
-              <CalendarDays className="mr-2 h-4 w-4 opacity-70" />{" "}
-              <span className="text-sm text-muted-foreground">
-                Joined January 2023
-              </span>
+          <HoverCardTrigger asChild>
+            <Link
+              href="https://read.cv/hackshaw"
+              className="underline hover:text-blue-700 transition-colors duration-200"
+            >
+              Read.cv
+            </Link>
+          </HoverCardTrigger>
+          <HoverCardContent className="w-80">
+            <div className="flex justify-between space-x-4">
+              <Avatar>
+                <AvatarImage src="/readcv.svg" />
+                <AvatarFallback>Read.cv</AvatarFallback>
+              </Avatar>
+              <div className="space-y-1">
+                <p className="text-sm font-semibold">Read.cv</p>
+                <p className="text-sm">
+                  Read.cv is a show, don&apos;t tell professional platform to
+                  form beautiful profiles and make meaningful connections with
+                  people and teams.
+                </p>
+                <div className="flex items-center pt-2">
+                  <CalendarDays className="mr-2 h-4 w-4 opacity-70" />{" "}
+                  <span className="text-sm text-muted-foreground">
+                    Joined January 2023
+                  </span>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </HoverCardContent>
-    </HoverCard>
+          </HoverCardContent>
+        </HoverCard>
         , and{" "}
         <HoverCard>
-      <HoverCardTrigger asChild>
-      <Link
-          href="https://merveilles.town/@tomupom"
-          className="underline hover:text-blue-700 transition-colors duration-200"
-        >
-          Merveilles
-        </Link>
-      </HoverCardTrigger>
-      <HoverCardContent className="w-80">
-        <div className="flex justify-between space-x-4">
-          <Avatar>
-            <AvatarImage src="/merveilles.png" />
-            <AvatarFallback>Merveilles</AvatarFallback>
-          </Avatar>
-          <div className="space-y-1">
-            <p className="text-sm font-medium">Merveilles</p>
-            <p className="text-sm">
-            Merveilles is a collective of forward-thinking individuals who strive to better each other and their surroundings through constant creation and play. This community project is aimed at the establishment of new ways of speaking, seeing and organizing. A warm welcome to any like-minded people who feel these ideals resonate with them.
-            </p>
-            <div className="flex items-center pt-2">
-              <CalendarDays className="mr-2 h-4 w-4 opacity-70" />{" "}
-              <span className="text-sm text-muted-foreground">
-                Joined May 2019
-              </span>
+          <HoverCardTrigger asChild>
+            <Link
+              href="https://merveilles.town/@tomupom"
+              className="underline hover:text-blue-700 transition-colors duration-200"
+            >
+              Merveilles
+            </Link>
+          </HoverCardTrigger>
+          <HoverCardContent className="w-80">
+            <div className="flex justify-between space-x-4">
+              <Avatar>
+                <AvatarImage src="/merveilles.png" />
+                <AvatarFallback>Merveilles</AvatarFallback>
+              </Avatar>
+              <div className="space-y-1">
+                <p className="text-sm font-semibold">Merveilles</p>
+                <p className="text-sm">
+                  Merveilles is a collective of forward-thinking individuals who
+                  strive to better each other and their surroundings through
+                  constant creation and play. This community project is aimed at
+                  the establishment of new ways of speaking, seeing and
+                  organizing. A warm welcome to any like-minded people who feel
+                  these ideals resonate with them.
+                </p>
+                <div className="flex items-center pt-2">
+                  <CalendarDays className="mr-2 h-4 w-4 opacity-70" />{" "}
+                  <span className="text-sm text-muted-foreground">
+                    Joined May 2019
+                  </span>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </HoverCardContent>
-    </HoverCard>
+          </HoverCardContent>
+        </HoverCard>
         . Sometimes, I am on IRC as @tomupom on Libera Chat and Rizon networks.
-        You can also find me on Urbit as ~worbur-dorneb.
+        You can also find me on{" "}
+        <HoverCard>
+          <HoverCardTrigger asChild>
+            <Link
+              href="https://urbit.org"
+              className="underline hover:text-blue-700 transition-colors duration-200"
+            >
+              Urbit
+            </Link>
+          </HoverCardTrigger>
+          <HoverCardContent className="w-80">
+            <div className="flex justify-between space-x-4">
+              <Avatar>
+                <AvatarImage src="/urbit.webp" />
+                <AvatarFallback>Urbit</AvatarFallback>
+              </Avatar>
+              <div className="space-y-1">
+                <p className="text-sm font-semibold">Urbit</p>
+                <p className="text-sm">
+                  Urbit is a decentralized personal server platform based on
+                  functional programming in a peer-to-peer network.
+                </p>
+                <div className="flex items-center pt-2">
+                  <CalendarDays className="mr-2 h-4 w-4 opacity-70" />{" "}
+                  <span className="text-sm text-muted-foreground">
+                    Joined January 2019
+                  </span>
+                </div>
+              </div>
+            </div>
+          </HoverCardContent>
+        </HoverCard>{" "}
+        as ~worbur-dorneb.
       </p>
       <p className="pb-4">
         I have accounts on other popular social media sites, but rarely use
@@ -180,7 +255,14 @@ export default function About() {
       <p className="pb-8">
         I would also like to acknowledge the many free and open source software
         that this website, along with many of my other projects, take advantage
-        of.
+        of. The full source code of this website can be found on my{" "}
+        <Link
+          href="https://github.com/et0and/tom.so"
+          className="underline hover:text-blue-700 transition-colors duration-200"
+        >
+          GitHub
+        </Link>
+        .
       </p>
     </>
   );
