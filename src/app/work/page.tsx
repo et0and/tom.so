8494b61b74f4d6845bfc2fe816c81d88d9f3df2d
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2024-06-27 23:37:27 +1200

    Removed banner for now

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -11,33 +11,36 @@ export default function WorkPage() {
 
   return (
     <>
-    <div className="w-full">
-      {allWorks
-        .sort((a, b) => {
-          if (a.metadata.title.toLowerCase() < b.metadata.title.toLowerCase()) {
-            return -1;
-          }
-          if (a.metadata.title.toLowerCase() > b.metadata.title.toLowerCase()) {
-            return 1;
-          }
-          return 0;
-        })
-        .map((post) => (
-          <Link
-            key={post.slug}
-            className="flex flex-col hover:text-blue-700 transition-colors duration-200 space-y-1 mb-4"
-            href={`/work/${post.slug}`}
-          >
-            <div className="w-full flex flex-col">
-              <p className="text-2xl font-medium tracking-tighter">
-                {post.metadata.title}
-              </p>
-              <p className="text-lg">{post.metadata.summary}</p>
-            </div>
-            
-          </Link>
-        ))}
-    </div>
+      <div className="w-full items-center">
+        {allWorks
+          .sort((a, b) => {
+            if (
+              a.metadata.title.toLowerCase() < b.metadata.title.toLowerCase()
+            ) {
+              return -1;
+            }
+            if (
+              a.metadata.title.toLowerCase() > b.metadata.title.toLowerCase()
+            ) {
+              return 1;
+            }
+            return 0;
+          })
+          .map((post) => (
+            <Link
+              key={post.slug}
+              className="flex flex-col hover:text-blue-700 transition-colors duration-200 space-y-1 mb-4"
+              href={`/work/${post.slug}`}
+            >
+              <div className="w-full flex flex-col">
+                <p className="text-2xl font-medium tracking-tighter">
+                  {post.metadata.title}
+                </p>
+                <p className="text-lg">{post.metadata.summary}</p>
+              </div>
+            </Link>
+          ))}
+      </div>
     </>
   );
 }
