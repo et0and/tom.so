88708b190f663d586b673fdb0595edd29c17c0e5
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2024-07-04 23:45:05 +1200

    no array map

--- src/app/posts/loading.tsx
+++ src/app/posts/loading.tsx
@@ -7,8 +7,6 @@ export default function Loading() {
         <div className="h-4 bg-gray-200 rounded w-full mb-4"></div>
         <Separator className="my-4" />
         <div className="space-y-4 pb-8">
-          {[...Array(5)].map((_, index) => (
-            <div key={index} className="space-y-2">
               <div className="h-4 bg-gray-200 rounded w-full"></div>
               <div className="h-4 bg-gray-200 rounded w-5/6"></div>
               <div className="h-4 bg-gray-200 rounded w-4/6"></div>
--- src/app/work/loading.tsx
+++ src/app/work/loading.tsx
@@ -6,9 +6,7 @@ export default function Loading() {
         <div className="h-8 bg-gray-200 rounded w-3/4 mb-2"></div>
         <div className="h-4 bg-gray-200 rounded w-full mb-4"></div>
         <Separator className="my-4" />
-        <div className="space-y-4 pb-8">
-          {[...Array(5)].map((_, index) => (
-            <div key={index} className="space-y-2">
+        <div className="space-y-4 pb-
               <div className="h-4 bg-gray-200 rounded w-full"></div>
               <div className="h-4 bg-gray-200 rounded w-5/6"></div>
               <div className="h-4 bg-gray-200 rounded w-4/6"></div>
