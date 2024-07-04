75afd836f6ad4a19444824cb7f8be28078caa712
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2024-07-04 23:32:56 +1200

    no section for loading state

--- src/app/work/[slug]/loading.tsx
+++ src/app/work/[slug]/loading.tsx
@@ -2,7 +2,6 @@ import { Separator } from "@/components/ui/separator";
 
 export default function Loading() {
   return (
-    <section>
       <div className="animate-pulse">
         <div className="h-8 bg-gray-200 rounded w-3/4 mb-2"></div>
         <div className="h-4 bg-gray-200 rounded w-full mb-4"></div>
@@ -17,6 +16,5 @@ export default function Loading() {
           ))}
         </div>
       </div>
-    </section>
   );
 }
