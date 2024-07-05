a046b6b8122ce695465c09905c22bd78fccecad9
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2024-07-05 03:26:50 +0000

    I forgor the image container skeleton

--- src/app/work/loading.tsx
+++ src/app/work/loading.tsx
@@ -3,17 +3,17 @@ import { Skeleton } from "@/components/ui/skeleton"
 export default function Loading() {
   return (
     <div className="flex flex-col space-y-3">
-      <Skeleton className="h-[350px] w-[550px] rounded-lg" />
+      <Skeleton className="h-[350px] w-full rounded-lg" />
       <div className="space-y-2">
         <Skeleton className="h-4 w-full" />
         <Skeleton className="h-4 w-full" />
       </div>
-      <Skeleton className="h-[350px] w-[550px] rounded-lg pt-4" />
+      <Skeleton className="h-[350px] w-full rounded-lg pt-4" />
       <div className="space-y-2">
         <Skeleton className="h-4 w-full" />
         <Skeleton className="h-4 w-full" />
       </div>
-      <Skeleton className="h-[350px] w-[550px] rounded-lg pt-4" />
+      <Skeleton className="h-[350px] w-full rounded-lg pt-4" />
       <div className="space-y-2">
         <Skeleton className="h-4 w-full" />
         <Skeleton className="h-4 w-full" />
