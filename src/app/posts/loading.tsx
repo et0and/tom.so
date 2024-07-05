312294c03dd452fd545e198bcd538dd40c77b288
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2024-07-05 03:36:26 +0000

    Fixed ui

--- src/app/posts/loading.tsx
+++ src/app/posts/loading.tsx
@@ -3,17 +3,17 @@ import { Skeleton } from "@/components/ui/skeleton"
 export default function Loading() {
   return (
     <div className="flex flex-col space-y-3">
-      <Skeleton className="h-[350px] w-full rounded-lg" />
+      <Skeleton className="h-[350px] w-[400px] rounded-lg" />
       <div className="space-y-2">
         <Skeleton className="h-4 w-full" />
         <Skeleton className="h-4 w-full" />
       </div>
-      <Skeleton className="h-[350px] w-full rounded-lg pt-4" />
+      <Skeleton className="h-[350px] w-[400px] rounded-lg pt-4" />
       <div className="space-y-2">
         <Skeleton className="h-4 w-full" />
         <Skeleton className="h-4 w-full" />
       </div>
-      <Skeleton className="h-[350px] w-full rounded-lg pt-4" />
+      <Skeleton className="h-[350px] w-[400px] rounded-lg pt-4" />
       <div className="space-y-2">
         <Skeleton className="h-4 w-full" />
         <Skeleton className="h-4 w-full" />
