8b85a957547ae69fb53c9cd741b471546ea8654a
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2024-07-05 03:19:17 +0000

    Fixed loading ui to be responsive

--- src/app/posts/loading.tsx
+++ src/app/posts/loading.tsx
@@ -5,18 +5,18 @@ export default function Loading() {
     <div className="flex flex-col space-y-3">
       <Skeleton className="h-[350px] w-[550px] rounded-lg" />
       <div className="space-y-2">
-        <Skeleton className="h-4 w-[500px]" />
-        <Skeleton className="h-4 w-[500px]" />
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-full" />
       </div>
       <Skeleton className="h-[350px] w-[550px] rounded-lg pt-4" />
       <div className="space-y-2">
-        <Skeleton className="h-4 w-[500px]" />
-        <Skeleton className="h-4 w-[500px]" />
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-full" />
       </div>
       <Skeleton className="h-[350px] w-[550px] rounded-lg pt-4" />
       <div className="space-y-2">
-        <Skeleton className="h-4 w-[500px]" />
-        <Skeleton className="h-4 w-[500px]" />
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-full" />
       </div>
     </div>
   );
