0d1118ad8b7cc6223551a9f71631890d4c6b6838
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2024-07-05 03:11:21 +0000

    Loading skeleton for rendering

--- src/app/posts/loading.tsx
+++ src/app/posts/loading.tsx
@@ -1,17 +1,23 @@
-import { Separator } from "@/components/ui/separator";
+import { Skeleton } from "@/components/ui/skeleton"
 
 export default function Loading() {
   return (
-      <div className="animate-pulse">
-        <div className="h-8 bg-gray-200 rounded w-3/4 mb-2"></div>
-        <div className="h-4 bg-gray-200 rounded w-full mb-4"></div>
-        <Separator className="my-4" />
-        <div className="space-y-4 pb-8">
-              <div className="h-4 bg-gray-200 rounded w-full"></div>
-              <div className="h-4 bg-gray-200 rounded w-5/6"></div>
-              <div className="h-4 bg-gray-200 rounded w-4/6"></div>
-            </div>
-        </div>
+    <div className="flex flex-col space-y-3">
+      <Skeleton className="h-[350px] w-[550px] rounded-lg" />
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-[500px]" />
+        <Skeleton className="h-4 w-[500px]" />
       </div>
+      <Skeleton className="h-[350px] w-[550px] rounded-lg pt-4" />
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-[500px]" />
+        <Skeleton className="h-4 w-[500px]" />
+      </div>
+      <Skeleton className="h-[350px] w-[550px] rounded-lg pt-4" />
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-[500px]" />
+        <Skeleton className="h-4 w-[500px]" />
+      </div>
+    </div>
   );
 }
