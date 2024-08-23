ade0b47642ce3ef04801c3f85d149264bed7d1af
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2024-08-23 15:15:16 +1200

    next/image not optimised for arena grid

--- src/components/ui/in-view-images-grid.tsx
+++ src/components/ui/in-view-images-grid.tsx
@@ -89,6 +89,7 @@ export function InViewImagesGrid({ channelSlug }: InViewImagesGridProps) {
                   >
                     <Image
                       src={imgSrc}
+                      unoptimized
                       alt={`Image from Are.na channel ${channelSlug}, index:${index}`}
                       className="bg-slate-400 grayscale size-full rounded-lg object-contain"
                       width={500}
