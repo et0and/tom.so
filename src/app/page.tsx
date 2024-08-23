c8288d3a86999ce1c573d36d5b161999a436047a
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2024-08-23 18:58:40 +1200

    Fancy trailing image on homepage (desktop)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import Image from "next/image";
+import TrailingImage from "@/components/ui/trailing-image";
 
 export const metadata: Metadata = {
   openGraph: {
@@ -17,13 +18,8 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
-      <div className="flex-grow items-center justify-center transition grayscale dark:invert hidden md:flex">
-        <Image
-          src="/d.jpg"
-          width={500}
-          height={500}
-          alt="Highly grainy photograph of a classroom that I used to teach in at Westlake Boys High School."
-        />
+      <div className="flex-grow items-center justify-center transition grayscale hidden md:flex">
+        <TrailingImage />
       </div>
     </div>
   );
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,16 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+// Linear interpolation
+export function lerp(a: number, b: number, n: number) {
+  return (1 - n) * a + n * b;
+}
+
+// Get distance between two points
+export function getDistance(x1: number, y1: number, x2: number, y2: number) {
+  return Math.hypot(x2 - x1, y2 - y1);
+}
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
