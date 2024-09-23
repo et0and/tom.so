c06010bb9d95b42064ac9b140f776efaa78a358f
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2024-09-23 20:51:35 +1200

    Font styling OG images

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -3,15 +3,29 @@ import { NextRequest } from "next/server";
 
 export const runtime = "edge";
 
+// Load the fonts
+const geistSansRegular = fetch(
+  new URL("../../assets/fonts/geist/static/Geist-Regular.ttf", import.meta.url)
+).then((res) => res.arrayBuffer());
+
+const geistSansSemibold = fetch(
+  new URL("../../assets/fonts/geist/static/Geist-SemiBold.ttf", import.meta.url)
+).then((res) => res.arrayBuffer());
+
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const title = searchParams.get("title");
+
+  // Wait for the fonts to load
+  const [geistSansRegularData, geistSansSemiboldData] = await Promise.all([
+    geistSansRegular,
+    geistSansSemibold,
+  ]);
 
   return new ImageResponse(
     (
       <div
         style={{
-          fontSize: 50,
           background: "white",
           width: "100%",
           height: "100%",
@@ -23,13 +37,45 @@ export async function GET(request: NextRequest) {
           justifyContent: "flex-start",
         }}
       >
-        <p style={{ margin: 0, marginBottom: 20 }}>{title}</p>
-        <p style={{ margin: 0, fontSize: 40 }}>Tom Hackshaw</p>
+        <p
+          style={{
+            fontFamily: '"Geist Sans Semibold"',
+            fontSize: 50,
+            margin: 0,
+            marginBottom: 5,
+            fontWeight: 600,
+          }}
+        >
+          {title}
+        </p>
+        <p
+          style={{
+            fontFamily: '"Geist Sans"',
+            fontSize: 40,
+            margin: 0,
+          }}
+        >
+          Tom Hackshaw
+        </p>
       </div>
     ),
     {
       width: 1200,
       height: 630,
-    },
+      fonts: [
+        {
+          name: "Geist Sans",
+          data: geistSansRegularData,
+          style: "normal",
+          weight: 400,
+        },
+        {
+          name: "Geist Sans Semibold",
+          data: geistSansSemiboldData,
+          style: "normal",
+          weight: 600,
+        },
+      ],
+    }
   );
 }
