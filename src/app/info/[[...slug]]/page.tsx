9a235031e167039ddc7e4be39d21152cb4a946f3
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2024-10-14 20:28:03 +1300

    Performance refactor

--- src/app/info/[[...slug]]/page.tsx
+++ src/app/info/[[...slug]]/page.tsx
@@ -87,7 +87,7 @@ export default async function InfoPage({
                   })}
                   : {count} views
                 </li>
-              )
+              ),
             )}
           </ul>
         </>
