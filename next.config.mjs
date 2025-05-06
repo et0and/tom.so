c9268649e580451f193e9e0359babe2cc989ed9c
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2025-05-06 10:31:43 +0000

    fix: config csps

--- next.config.mjs
+++ next.config.mjs
@@ -20,17 +20,45 @@ const nextConfig = {
   },
   async rewrites() {
     return [
+      // Rewrite for cars.tom.so
+      {
+        source: '/tomica',
+        destination: 'https://cars.tom.so',
+      },
       {
         source: '/tomica/:path*',
         destination: 'https://cars.tom.so/:path*',
+      },
+      // Rewrite for ui.tom.so
+      {
+        source: '/ui',
+        destination: 'https://ui.tom.so',
       },
       {
         source: '/ui/:path*',
         destination: 'https://ui.tom.so/:path*',
       },
+      // Rewrite for good-sign-offs.work.tom.so
       {
-        source: '/good-sign-offs/:path*',
+        source: '/sign-offs',
+        destination: 'https://good-sign-offs.work.tom.so',
+      },
+      {
+        source: '/sign-offs/:path*',
         destination: 'https://good-sign-offs.work.tom.so/:path*',
+      },
+      // Special rewrites for _next paths (for CSS/JS assets)
+      {
+        source: '/tomica/_next/:path*',
+        destination: 'https://cars.tom.so/_next/:path*',
+      },
+      {
+        source: '/ui/_next/:path*',
+        destination: 'https://ui.tom.so/_next/:path*',
+      },
+      {
+        source: '/sign-offs/_next/:path*',
+        destination: 'https://good-sign-offs.work.tom.so/_next/:path*',
       },
     ];
   },
@@ -50,16 +78,16 @@ const nextConfig = {
 };
 
 const ContentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline';
-  style-src 'self' 'unsafe-inline' *.googleapis.com *.gstatic.com;
+  default-src 'self' *.tom.so;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.tom.so;
+  style-src 'self' 'unsafe-inline' *.googleapis.com *.gstatic.com *.tom.so;
   img-src 'self' data: https:;
-  font-src 'self' *.gstatic.com *.googleapis.com ;
+  font-src 'self' *.gstatic.com *.googleapis.com *.tom.so;
   object-src 'none';
   base-uri 'self';
-  form-action 'self';
+  form-action 'self' *.tom.so;
   frame-ancestors 'none';
-  connect-src 'self' *.github.com *.keystatic.com *.gstatic.com *.googleapis.com *.githubusercontent.com cars.tom.so performance.vercel.app;
+  connect-src 'self' *.github.com *.keystatic.com *.gstatic.com *.googleapis.com *.githubusercontent.com *.tom.so;
   block-all-mixed-content;
   upgrade-insecure-requests;
 `;
