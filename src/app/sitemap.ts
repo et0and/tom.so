e54a5c963eecbad0c8800f3326fd11beae45a7e4
Tom Hackshaw <gh@tomhackshaw.com>
2025-06-05 19:50:46 +1200

    fix: fix sitemap generation

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,25 +1,26 @@
 import { getBlogPosts } from "@/app/db/blog";
 import { getWorkPosts } from "@/app/db/work";
+import { MetadataRoute } from "next";
 
-export default async function sitemap() {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = "https://tom.so";
 
   // Static routes
   let routes = ["", "/posts", "/about", "/work"].map((route) => ({
     url: `${baseUrl}${route}`,
-    lastModified: new Date().toISOString().split("T")[0],
+    lastModified: new Date(),
   }));
 
   // Blog posts
   const blogPosts = getBlogPosts().map((post) => ({
     url: `${baseUrl}/posts/${post.slug}`,
-    lastModified: post.metadata.publishedAt,
+    lastModified: new Date(post.metadata.publishedAt),
   }));
 
   // Work posts
   const workPosts = getWorkPosts().map((post) => ({
     url: `${baseUrl}/work/${post.slug}`,
-    lastModified: post.metadata.publishedAt,
+    lastModified: new Date(post.metadata.publishedAt),
   }));
 
   return [...routes, ...blogPosts, ...workPosts];
